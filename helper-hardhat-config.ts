835941d15ca990d474b5d71d9c63facc8ed8b660
pongtsu <alwayshungrytolearn@gmail.com>
2022-08-12 07:28:52 +0700

    feat: Deploy nft script.

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -2,6 +2,7 @@ export interface networkConfigItem {
   ethUsdPriceFeed?: string
   blockConfirmation?: number
   chainId?: number
+  name?: string
   subscriptionId?: string
   gasLane?: string
   keepersUpdateInterval?: string
@@ -14,12 +15,13 @@ export interface networkConfigItem {
 }
 
 export interface networkConfigInfo {
-  [key: string]: networkConfigItem
+  [key: number]: networkConfigItem
 }
 
 export const networkConfig: networkConfigInfo = {
-  hardhat: {
+  31337: {
     chainId: 31337,
+    name: "localhost",
     subscriptionId: "588",
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
@@ -29,21 +31,11 @@ export const networkConfig: networkConfigInfo = {
     fundAmount: "1000000000000000000000",
     waitBlockConfirmations: 1,
   },
-  localhost: {
-    chainId: 31337,
-    subscriptionId: "588",
-    gasLane:
-      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-    keepersUpdateInterval: "30",
-    raffleEntranceFee: "100000000000000000", // 0.1 ETH
-    callbackGasLimit: "500000", // 500,000 gas
-    fundAmount: "1000000000000000000000",
-    waitBlockConfirmations: 1,
-  },
-  rinkeby: {
+  4: {
     ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
     blockConfirmation: 6,
     chainId: 4,
+    name: "rinkeby",
     subscriptionId: "588",
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
@@ -56,7 +48,7 @@ export const networkConfig: networkConfigInfo = {
   },
 }
 
-export const developmentChains = ["hardhat", "localhost"]
+export const developmentChainId = [31337]
 
 export const frontEndAbiFile = "../next-lottery/constants/abi.json"
 export const frontEndContractsFile =
