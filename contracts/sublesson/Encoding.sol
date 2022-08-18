f649af38ded82ddf630dfb6cb1f87fbbdc08d2d6
pongtsu <alwayshungrytolearn@gmail.com>
2022-08-18 19:05:38 +0700

    feat: Add string and bytes converter.

--- contracts/sublesson/Encoding.sol
+++ contracts/sublesson/Encoding.sol
@@ -67,4 +67,53 @@ contract Encoding {
     function stringCastFromEncode() public pure returns (string memory) {
         return string(abi.encode("pongtsu"));
     }
+
+    function stringConcat() public pure returns (string memory) {
+        return string.concat("pongtsu", "gibgyb");
+    }
+
+    function stringToBytesAccess()
+        public
+        pure
+        returns (
+            bytes1,
+            bytes1,
+            bytes1,
+            bytes1,
+            bytes1,
+            bytes1,
+            bytes1
+        )
+    {
+        string memory name = "pongtsu";
+        bytes memory nameInbytes = bytes(name);
+        return (
+            nameInbytes[0],
+            nameInbytes[1],
+            nameInbytes[2],
+            nameInbytes[3],
+            nameInbytes[4],
+            nameInbytes[5],
+            nameInbytes[6]
+        );
+    }
+
+    function charAccess() public pure returns (string memory) {
+        string memory name = "pongtsu";
+        bytes memory nameInbytes = bytes(name);
+        bytes memory display = new bytes(3);
+        display[0] = nameInbytes[0];
+        // return (string(nameInbytes[0]), string(nameInbytes[1]), string(nameInbytes[2]), string(nameInbytes[3]),string(nameInbytes[4]),string(nameInbytes[5]), string(nameInbytes[6]));
+        return string(display);
+    }
+
+    function getFirstChar(string memory _originString)
+        public
+        pure
+        returns (string memory)
+    {
+        bytes memory firstCharByte = new bytes(1);
+        firstCharByte[0] = bytes(_originString)[0];
+        return string(firstCharByte);
+    }
 }
